5856c9ec0523dfa7ca6edde761e285190acf47e1
ikbenlike <thewatcheriswatchinu@gmail.com>
2016-01-08 02:12:36 +0100

    Update metabot.py

--- metabot.py
+++ metabot.py
@@ -1,18 +1,24 @@
 import discord
+import requests
 
 client = discord.Client()
-
 user = os.environ.get("DISCORD_USER", None)
 password = os.environ.get("DISCORD_PASSWORD", None)
-
-if not user or not password:
-    raise Exception("Your Discord login details are missing inside the enviornment. " +
-                    "Please set DISCORD_USER and DISCORD_PASSWORD before running this bot.")
 
 @client.event
 def on_message(message):
     if message.content.startswith('$about'):
         client.send_message(message.channel, 'this bot can be used to read metadata from an image')
+    if message.content.startswith('$meme'):
+        client.send_message(message.channel, 'bit.ly/1ITzC4D')
+    if message.content.startswith('$summary'):
+        client.send_message(message.channel, 'read #summary before asking stuff!')
+    if message.content.startswith('$wiki'):
+        client.send_message(message.channel, 'http://wiki.databutt.com/index.php?title=Main_Page')
+    if message.content.startswith('$ource'):
+        client.send_message(message.channel, 'sourcecode: https://github.com/ikbenlike/metabot.git')
+    if message.content.startswith('$ping'):
+        client.send_message(message.channel, 'pong')
 
 @client.event
 def on_ready():
