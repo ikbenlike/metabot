a61cd267fb67b6cb7183b14997aea1605957c5cc
ikbenlike <thewatcheriswatchinu@gmail.com>
2016-01-09 14:07:40 +0100

    Update metabot.py

--- metabot.py
+++ metabot.py
@@ -20,23 +20,23 @@ def on_message(message):
     if message.content.startswith('$ping'):
         client.send_message(message.channel, 'pong')
     if message.content.startswith('$meta'):
-	    client.send_message(message.channel, 'this feature will be implemented later on')
-	if message.content.startswith('$commands'):
-		client.send_message(message.channel, 'this feature will be implemented later on')
-	if message.content.startswith('$upvote'):
-		client.send_message(message.channel, '+1')
-	if message.content.startswith('$downvote'):
-		client.send_message(message.channel, '-1')
-	if message.content.startswith('$repost'):
-		client.send_message(message.channel, 'thats a repost bitch')
-	if message.content.startswith('$linux'):
-		client.send_message(message.channel, 'https://www.codecademy.com/learn/learn-the-command-line')
-	if message.content.startswith('!bots'):
-		client.send_message(message.channel, '``` bot that will eventually be able to get metadata from\nimages ```')
-	if message.content.startswith('$bots'):
-		client.send_message(message.channel, '``` bot that will eventually be able to get metadata from\nimages ```')
-	if message.content.startswith('$new'):
-		client.send_message(message.channel, '``` \n read #rules for the rules \n read #summary for the summary \n read tools for useful tools \n ```')
+	client.send_message(message.channel, 'this feature will be implemented later on')
+    if message.content.startswith('$commands'):
+        client.send_message(message.channel, 'this feature will be implemented later on')
+    if message.content.startswith('$upvote'):
+	client.send_message(message.channel, '+1')
+    if message.content.startswith('$downvote'):
+	client.send_message(message.channel, '-1')
+    if message.content.startswith('$repost'):
+	client.send_message(message.channel, 'thats a repost bitch')
+    if message.content.startswith('$linux'):
+	client.send_message(message.channel, 'https://www.codecademy.com/learn/learn-the-command-line')
+    if message.content.startswith('!bots'):
+	client.send_message(message.channel, '``` bot that will eventually be able to get metadata from\nimages ```')
+    if message.content.startswith('$bots'):
+	client.send_message(message.channel, '``` bot that will eventually be able to get metadata from\nimages ```')
+    if message.content.startswith('$new'):
+        client.send_message(message.channel, '``` \n read #rules for the rules \n read #summary for the summary \n read tools for useful tools \n ```')
 
 @client.event
 def on_ready():
